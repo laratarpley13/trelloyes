ed69686103069603d765bfaba9c397aede260afe
laratarpley13 <ltarp13@gmail.com>
2020-12-10 15:15:38 -0600

    card function added

--- src/Card.js
+++ src/Card.js
@@ -1,2 +1,16 @@
 import React from 'react';
 
+//Accepts two props title and component
+//title is a string of the card's title
+//content is a string of the card's content
+//These props will be passed in for each Card from the List component
+
+function Card(props) {
+    return (
+        <div className="Card">
+            <button type="button">delete</button>
+            <h3>{props.title}</h3>
+    <       p>{props.content}</p>
+        </div>
+    );
+}
