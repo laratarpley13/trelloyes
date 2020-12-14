8c2c6ac0d4b32f3758dc35e65e274b825f08fd67
laratarpley13 <ltarp13@gmail.com>
2020-12-14 13:39:11 -0600

    added test for Lists and Cards

--- src/List.js
+++ src/List.js
@@ -21,7 +21,7 @@ function List (props) {
                         content={card.content}
                     />
                 )}
-                <button type="button" classType="List-add-button">
+                <button type="button" className="List-add-button">
                     + Add Random Card
                 </button>    
             </div>
